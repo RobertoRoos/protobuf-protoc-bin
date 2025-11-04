33f102640d07202b1b0e1e26cb0a1198e1d08011
RobertoRoos <robert.soor@gmail.com>
2025-11-04 12:10:43 +0100

    Removed testing debug info

--- setup.py
+++ setup.py
@@ -154,11 +154,6 @@ class CustomWheel(bdist_wheel):
 
         See https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/
         """
-
-        print("sysconfig.get_platform():", sysconfig.get_platform())
-        print("platform.system():", platform.system())
-        print("platform.architecture():", platform.architecture())
-
         impl_tag = "py2.py3"  # Same wheel across Python versions
         abi_tag = "none"  # Same wheeel across ABI versions (not a C-extension)
         # But we need to differentiate on the platform for the protoc binary:
@@ -171,12 +166,7 @@ class CustomWheel(bdist_wheel):
         # MacOS platform tags area already okay
 
         # We also keep Windows tags in place, instead of using `any`, to prevent an
-        # obscure Linux platform to getting an incompatible wheel
-
-        print("Info:")
-        print("impl_tag:", impl_tag)
-        print("abi_tag:", abi_tag)
-        print("plat_tag:", plat_tag)
+        # obscure Linux platform to getting an incompatible binary
 
         return impl_tag, abi_tag, plat_tag
 
